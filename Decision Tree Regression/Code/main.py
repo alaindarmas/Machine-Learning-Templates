665980e60d1ccd2528527291f31c94f9d2e3617c
alaindarmas <alaindarmas@gmail.com>
2024-07-26 15:45:05 -0400

    Initial commit to Machine Learning Templates Decision Tree Regression

---
+++ Decision Tree Regression/Code/main.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+# Relative path to the config file
+config_file_relative_path = 'Documentation/ConfigFile.xlsx'
+
+# Get the absolute path to the config file
+config_file_path = os.path.abspath(config_file_relative_path)
+
+# Read the config file
+config = pd.read_excel(config_file_path)
+config_dict = pd.Series(config.Value.values, index=config.Key).to_dict()
+
+# Convert paths to relative
+base_path = os.path.dirname(config_file_path)
+dataset_path = os.path.abspath(os.path.join(base_path, config_dict['dataset_path']))
+output_path = os.path.abspath(os.path.join(base_path, config_dict['output_path']))
+
+# Load dataset
+dataset = pd.read_excel(dataset_path)
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+from sklearn.metrics import r2_score
+print("R2 Score:", r2_score(y_test, y_pred))
+
+# Save output
+output_file = os.path.join(output_path, 'output_results.csv')
+output_data = np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1)
+output_df = pd.DataFrame(output_data, columns=['Predicted', 'Actual'])
+output_df.to_csv(output_file, index=False)
